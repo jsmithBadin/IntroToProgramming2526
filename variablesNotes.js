7b0ab83900cc9172a77d7dd23fa572a736ba2af0
jsmithBadin <135791484+jsmithBadin@users.noreply.github.com>
2025-08-26 14:31:51 -0400

    added notes about variables

--- variablesNotes.js
+++ variablesNotes.js
@@ -1,3 +1,15 @@
 // This is a comment.  This is visible to the user, but not to the computer itself.  They're very useful.  
-// Test to see if git is working the way I expect.
-// Test 3
+
+// a variable is essentially an invisible box.  You can put whatever you want into the box
+// and then if you "call" the variable, it will tell you what it contains.  
+
+// let is a command to create a new variable.  It pulls a box out of the aether and gives it a name.  
+
+let fish;
+fish = 3;
+
+let cats = 7;
+
+console.log(fish);
+console.log(cats);
+console.log(cats + fish);
