4644889ad99fc1689d8b158da3c093652c727b4e
jsmithBadin <135791484+jsmithBadin@users.noreply.github.com>
2025-09-02 14:26:08 -0400

    added notes

--- variablesNotes.js
+++ variablesNotes.js
@@ -5,11 +5,37 @@
 
 // let is a command to create a new variable.  It pulls a box out of the aether and gives it a name.  
 
-let fish;
-fish = 3;
+console.log("Hello, world!")
 
-let cats = 7;
+// let fish;
+// fish = 3;
 
-console.log(fish);
-console.log(cats);
-console.log(cats + fish);
+// let cats = 7;
+
+// console.log(fish);
+// console.log(cats);
+// console.log(cats + fish);
+
+
+// there are technically 3 kinds of variables in javascript.
+
+// let first = 1;
+// const second = 2;
+// var third = 3;
+
+// console.log(first);
+// console.log(second);
+// console.log(third);
+
+// first = 1 + 1;
+// console.log(first);
+
+// first = 7;
+// // second = 8;
+// third = 9;
+
+// console.log(first);
+// console.log(second);
+// console.log(third);
+
+// OPERATORS: +, -, *, /, ** (exponent), % (modulo)
