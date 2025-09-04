b9434342ac1443535aed89da60e68842b07cb62b
jsmithBadin <135791484+jsmithBadin@users.noreply.github.com>
2025-09-04 14:22:39 -0400

    added notes on variable assignment and math operators

--- variablesNotes.js
+++ variablesNotes.js
@@ -39,6 +39,39 @@
 // console.log(third);
 
 // OPERATORS: +, -, *, /, ** (exponent), % (modulo)
-console.log(3 + 5);
-console.log(8 / 3);
-console.log((35 + 5) / 4);
+// console.log(3 + 5);
+// console.log(8 / 3);
+// console.log((35 + 5) / 4);
+
+// Add this statement to the script tag: let a = 10.
+// Below it, add console.log(a). When you run this, the browser console should log 10.
+// Afterwards, re-assign a with a different number value. 
+// Log a again afterwards and it should show the updated value 
+// (the previous log should still show the old value of 10 since 
+// that was before a was re-assigned).
+// Now add to the bottom of the script let b = 7 * a.
+// Log what b is. It should log the result of 7 multiplied by whatever you re-assigned a with.
+
+
+
+// Try this sequence:
+// Declare a const variable max with the value 57.
+// Declare another const variable actual and assign it max - 13.
+// Declare another const variable percentage and assign it actual / max.
+// Now if you log percentage, you should see a value in the console like 0.7719.
+
+
+let num1 = 1;
+
+//ways to change a variable:
+num1 = 1 + num1;  // increase the value of num1 by 1
+num1++;  // increase the value of num1 by 1
+num1 += 2; //increase the value of num1 by 2
+num1 *=2;  // multiply num1 by 2
+num1 -= 3; //subtract 3 from num1
+num1 /= 4; // divide num1 by 4
+
+// thisIsCamelCase
+// this_is_snake_case 
+// number is already programmed into javascript.
+
