06fb32707b02bc91c1387f9be7f983295e4b6c83
jsmithBadin <135791484+jsmithBadin@users.noreply.github.com>
2025-09-08 14:27:11 -0400

    did notes on number and bigInt

--- dataTypeNotes.js
+++ dataTypeNotes.js
@@ -5,6 +5,27 @@
 // in a given variable.
 
 //example:
-let message = "hello";
-message = 123;
+// let message = "hello";
+// message = 123;
 
+// // Number
+// let n = 5;                      // "int", integer
+// console.log(typeof(n));
+// n = 254.23589752378;            // "float", floating point number (decimals)
+// console.log(typeof(n));
+//javascript lumps int and float into a single data type, "number".
+
+// There are some special values included in the number datatype:
+// Infinity, -Infinity, NaN  (NaN is "Not a Number");
+// Mathematical operations in JavaScript is "safe", you can do anything
+
+// console.log(1 / 0);
+
+
+// BigInt, Big Integer, represents Big... Integers...
+// "number" as a data type can't store an integer value larger than 
+// (2^53 - 1) : 9,007,199,254,740,991
+
+const sampleBigInt = 123456789123456789123456789123456789n;
+
+// final "n" indicates that it's a bigInt
