8f6de1fe80f6a76639df79f3e0e99e360be81a7f
jsmithBadin <135791484+jsmithBadin@users.noreply.github.com>
2025-09-09 13:15:13 -0400

    finished notes on data types

--- dataTypeNotes.js
+++ dataTypeNotes.js
@@ -29,3 +29,56 @@
 const sampleBigInt = 123456789123456789123456789123456789n;
 
 // final "n" indicates that it's a bigInt
+
+// String - text.  There are three ways to produce strings in JavaScript
+// which are slightly different.
+
+let string1 = 'this is a string.'
+let string2 = "this is also a string."
+
+// you usually select what string notation you're using based on the content of 
+// your string.  For example:
+
+//  let dialogue = "The man yelled, "Here we go!""   (won't work.)
+let dialogue = 'The man yelled, "Here we go!"'
+
+// If your text will include apostrophes, use double double quotes.  If your text
+// will include dialogue, using single quotes.  
+
+// Functionally, single and double quotes are the same.  The third method 
+// of writing strings is fundamentally different.  Example:
+
+let name = "Steve";
+
+// alert(`Hello, ${name}!`);
+
+// Backticks allow you to embed code into the string
+
+console.log(`3 + 4 is equal to ${3 + 4}`);
+
+// char doesn't exist in JavaScript, but does exist in C and Java. 
+// it's basically a string of length 1.  
+
+// Boolean Values:  named for George Boole.  has 2 values: true and false.  
+let nameFieldValid = true;  // the person input a valid name
+let ageFieldValid = false;  // the person did not input a valid age
+
+console.log(3 > 1);
+
+// null: null contains only 1 value: "null".  "nothing", "empty", "value unknown";
+
+
+let age = null;
+
+
+// undefined: which means that a value has not been assigned.
+let fish;
+console.log(fish);
+
+
+// Objects and Symbols.  Objects can hold complex data structures.  Symbols
+// are used to identify objects.
+
+
+
+// Practice with strings:  
